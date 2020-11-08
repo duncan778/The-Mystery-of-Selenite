e0b420d55dab470f02d0915ff6a5d853a50dd057
pavel <pavel@Acer.Dlink>
2020-11-08 08:49:33 +0300

    Test Terrain and player movement

---
+++ Assets/Scripts/PlayerControl/PlayerJumping.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumping : MonoBehaviour
+{
+    // Animator playerAn;
+    Rigidbody playerRb;
+    [SerializeField] float jumpForce = 5f;
+    bool isOnGround = true;
+    public bool IsGameOver { get; set; }
+
+    void Start()
+    {
+        // playerAn = GetComponent<Animator>();
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (!IsGameOver)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+            {
+                // playerAn.SetTrigger("Jump");
+                playerRb.AddForce(new Vector3(0, jumpForce, 0) , ForceMode.Impulse);
+            }
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision other) 
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionStay(Collision other) 
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerControl/PlayerMovingRB.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class PlayerMovingRB : MonoBehaviour
+{
+    Rigidbody playerRb;
+    GameObject playerVisual;
+    // Animator playerAn;
+    [SerializeField] float walkSpeed = 1.5f;
+    [SerializeField] float rotSpeed = 1f;
+    public bool IsGameOver { get; set; }
+
+
+    void Start()
+    {
+        // playerAn = GetComponent<Animator>();
+        playerRb = GetComponent<Rigidbody>();
+        playerVisual = transform.GetChild(0).gameObject;
+    }
+
+    void FixedUpdate()
+    {
+        if (!IsGameOver)
+        {
+            float x = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            if (x != 0 || v != 0)
+            {
+                playerVisual.transform.LookAt(playerVisual.transform.position + new Vector3(x, 0, v));
+            }
+
+            playerRb.AddForce(playerVisual.transform.forward * Math.Abs(v) * walkSpeed * Time.deltaTime);
+            playerRb.AddForce(playerVisual.transform.forward * Math.Abs(x) * walkSpeed * Time.deltaTime);
+
+            if (v != 0 || x != 0)
+                {
+                    // playerAn.SetBool("Walk", true);
+                    // StepSound();
+                }
+            else
+            {
+                // playerAn.SetBool("Walk", false);
+            }
+            
+        }
+        else
+        {
+            // playerAn.SetBool("Walk", false);
+        }
+        
+    }
+
+
+
+}
