e0cc25846eb3a709d605807539d4294395d6148e
pavel <pavel@Acer.Dlink>
2020-11-15 10:36:18 +0300

    Stone Controller, environment update

---
+++ Assets/Scripts/StoneController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneController : MonoBehaviour
+{
+    [SerializeField] static int maxPennantCount = 10;
+    static int pennantCount = 0;
+    static GameObject stoneContainer;
+    public GameObject pennantPrefab;
+
+    private void Start() 
+    {
+        stoneContainer = GameObject.Find("Stones").gameObject;
+    }
+
+    private void OpenStone() 
+    {   
+        int pennantToFind = maxPennantCount - pennantCount;
+        if (pennantToFind > 0)
+        {
+            int stoneCount = stoneContainer.transform.childCount;
+            int range = stoneCount > pennantToFind ? stoneCount/pennantToFind : 1;
+            if (Random.Range(1, range) == 1)
+            {
+                Vector3 pennantPosition = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+                Instantiate(pennantPrefab, pennantPosition, pennantPrefab.transform.rotation);
+                pennantCount++;
+            }
+        }
+        //VFX
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            OpenStone();
+        }
+    }
+}
