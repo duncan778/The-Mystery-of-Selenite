24ca4d889f1e9cfa645d9737ecc54b81622e5b14
pavel <pavel@Acer.Dlink>
2020-11-26 10:09:08 +0300

    Lost gameOver procedure and start screen

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject startPanel;
+    public GameObject gameOverPanel;
+    private bool pauseMode = false;
+
+    void Start()
+    {
+        Time.timeScale = 0;
+        startPanel.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!pauseMode)
+            {
+                Time.timeScale = 0;
+                gameOverPanel.transform.GetChild(0).gameObject.GetComponent<Text>().text = "*** Pause ***";
+                gameOverPanel.SetActive(true);
+                pauseMode = true;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                gameOverPanel.SetActive(false);
+                pauseMode = false;
+            }
+        }
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void StartGame()
+    {
+        Time.timeScale = 1;
+        startPanel.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+       //
+    }
+}
--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -1,26 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class NPC : MonoBehaviour
 {
+    public GameObject gameOverPanel;
+
     public NPCState SelenitState { get; set; }
     public Transform[] wayPoints;
     [SerializeField] float speed = 1;
 
     public Transform workPoint;
+    public Transform panicPoint;
 
     private float currentTime;
     [SerializeField] float pauseTime = 3;
     [SerializeField] string workAnim;
+    [SerializeField] string panicAnim;
 
     CurveMotion selenit1;
+    CurveMotion panicWay;
+
     private Animator NPCAnimator;
 
     [SerializeField] Dialog dialogPr = null;
     private Dialog dialog;
 
-    private bool questCompleted = false;
+    [SerializeField] bool questCompleted = false;
 
     [SerializeField] string[] sentences;
     [SerializeField] string objectName = "";
@@ -123,7 +131,15 @@ public class NPC : MonoBehaviour
 
                 case NPCState.Panic:
                 {
-
+                    Debug.Log("Panic");
+                    bool result = panicWay.Move(panicPoint);
+                    if (result)
+                    {
+                        gameObject.SetActive(false);
+                        Time.timeScale = 0;
+                        gameOverPanel.transform.GetChild(0).gameObject.GetComponent<Text>().text = "You were discovered! Panic arose. Mission failed";
+                        gameOverPanel.SetActive(true);
+                    }
                 }
                 break;
 
@@ -136,12 +152,24 @@ public class NPC : MonoBehaviour
     {   
         if (other.CompareTag("Player") && !questCompleted) 
         {
+            if (SelenitState == NPCState.Panic)
+            {
+                return;
+            }
+
             //Panic Test
             if (iSelenit) //for Selenit NPC
             {
                 if (playerObjects.Scafandr && !playerEquipment.IsSelenit)
                 {
                     //Panic Action
+                    Transform[] panicPoints = new Transform[] {this.gameObject.transform, panicPoint};
+                    panicWay = new CurveMotion(this.gameObject, panicPoints);
+                    panicWay.MoveSetup(speed * 2, forward: true, cycled: false);
+                    SelenitState = NPCState.Panic;
+                    NPCAnimator.SetBool("Walk", false);
+                    NPCAnimator.SetBool(workAnim, false);
+                    NPCAnimator.SetBool(panicAnim, true);
                     return;
                 }
                 
@@ -152,6 +180,13 @@ public class NPC : MonoBehaviour
                                               || (!playerEquipment.IsSelenit && !playerObjects.Scafandr && playerObjects.Helmet))
                 {
                     //Panic action
+                    Transform[] panicPoints = new Transform[] {this.gameObject.transform, panicPoint};
+                    panicWay = new CurveMotion(this.gameObject, panicPoints);
+                    panicWay.MoveSetup(speed * 2, forward: true, cycled: false);
+                    SelenitState = NPCState.Panic;
+                    NPCAnimator.SetBool("Walk", false);
+                    NPCAnimator.SetBool(workAnim, false);
+                    NPCAnimator.SetBool(panicAnim, true);
                     return;
                 }
                 
@@ -167,6 +202,11 @@ public class NPC : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
+        if (SelenitState == NPCState.Panic)
+        {
+            return;
+        }
+
         if (other.CompareTag("Player"))
         {
             Destroy(dialog);
